07b9f7be7c1666ec219e27cd8a21a592fa0f27ed
Patryk Dudzinski <patryk@dudzinski.com.pl>
2021-10-18 22:23:36 +0200

    RWD poczatek - tablety i lapki

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './app.scss';
+import './mobile.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
